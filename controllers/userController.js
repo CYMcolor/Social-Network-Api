e3ce509ce6789ecc21490a809ad7764729e25143
Camille MacLaren <camillemaclaren@gmail.com>
2023-09-24 17:27:30 -0500

    add getSingleUser
    populate thoughts and friends caused 500 errors so they are commented out for now

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,28 @@ module.exports = {
       } catch (err) {
         res.status(500).json(err);
       }
-  },
+    },
+
+    // get single user by Id
+    async getSingleUser(req, res) {
+      try {
+        const user = await User.findOne({ _id: req.params.userId})
+          .select('-__v')
+          //.populate('thoughts')
+          //.populate('friends');
+
+        // cannot find user
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+
+        res.json(user);
+
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+
     // create a new user
     async createUser(req, res) {
         try {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 
 const {
     getUsers,
+    getSingleUser,
     createUser,
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser);
 
 module.exports = router;
